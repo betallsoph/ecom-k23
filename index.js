db5509355025a9c0290fe07959533f500b2f5e11
antt <2212662@dlu.edu.vn>
2025-06-26 20:35:42 +0700

    'first'

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
