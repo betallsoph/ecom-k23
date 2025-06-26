ce19f03eff84a8d79dc25b6f1b76ed7d543ecbcb
antt <2212662@dlu.edu.vn>
2025-06-27 02:23:21 +0700

    'v2'

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(5050, () => {
+    console.log('Server is running on port 5050');
 });
