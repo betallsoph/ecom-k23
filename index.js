559a84eaa1fb7699d13f0717be9074fee5a7d141
antt <2212662@dlu.edu.vn>
2025-07-03 18:48:32 +0700

    v3'

--- index.js
+++ index.js
@@ -2,14 +2,51 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://103.90.226.103:27017/k23-ecom';
+mongoose.connect(mongoURI).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+    console.log(err);
+});
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Welcome to K23-ECOM');
 });
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String,
+})
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.post('/products', async (req, res) => {
+    const { name, price, description } = req.body;
+    try {
+        const newProduct = new Product({ name, price, description });
+        await newProduct.save();
+        res.status(201).json(newProduct);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+})
+    
 
 app.listen(5050, () => {
     console.log('Server is running on port 5050');
